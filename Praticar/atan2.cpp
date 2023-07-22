783d23c874bd3bb97720d840d9b8aa5aef484b3a
Eduardo GM <eduardo.floyd@outlook.com>
2023-07-21 21:31:40 -0300

    Ajustados tabs de atan2.cpp

--- Praticar/atan2.cpp
+++ Praticar/atan2.cpp
@@ -4,26 +4,26 @@
 #include <math.h>
 
 int main() {
-    const int numPoints = 10;
-    const double minValue = -.00100;
-    const double maxValue = .00100;
-    const double stepSize = (maxValue - minValue) / (numPoints - 1);
+  const int numPoints = 10;
+  const double minValue = -.00100;
+  const double maxValue = .00100;
+  const double stepSize = (maxValue - minValue) / (numPoints - 1);
 
-    for (int i = 0; i < numPoints; ++i) {
-        double x = minValue + i * stepSize;
-		for (int j = 0; j < numPoints; ++j) {
-			double y = minValue + j * stepSize;
+  for (int i = 0; i < numPoints; ++i) {
+    double x = minValue + i * stepSize;
+    for (int j = 0; j < numPoints; ++j) {
+      double y = minValue + j * stepSize;
 
-			double angleRadians = std::atan2(y, x);
-			double angleDegrees = angleRadians * 180.0 / M_PI;
+      double angleRadians = atan2(y, x);
+      double angleDegrees = angleRadians * 180.0 / M_PI;
 
-			printf("(%10.6f, %10.6f)", x, y);
-			printf("\t\t rad = %9.6f", angleRadians); 
-			printf("\t\t angle = %9.3f", angleDegrees);
-			std::cout << std::endl;
-		}
+      printf("(%10.6f, %10.6f)", x, y);
+      printf("\t\t rad = %9.6f", angleRadians); 
+      printf("\t\t angle = %9.3f", angleDegrees);
+      std::cout << std::endl;
     }
+  }
 
-	_getch();
-    return 0;
+  _getch();
+  return 0;
 }
